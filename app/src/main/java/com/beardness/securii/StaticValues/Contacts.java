8389d7b05ea2d7ce827b8856ebb88daa7b86fb27
Andy <48250847+andybeardness@users.noreply.github.com>
2021-04-26 23:51:53 +0300

    Update Contacts.java

--- app/src/main/java/com/beardness/securii/StaticValues/Contacts.java
+++ app/src/main/java/com/beardness/securii/StaticValues/Contacts.java
@@ -2,7 +2,7 @@ package com.beardness.securii.StaticValues;
 
 public class Contacts {
   
-  public static final String GITHUB_URL = "https://github.com/andybeardness";
+  public static final String GITHUB_URL = "https://github.com/andybeardness/Android-Securii";
   public static final String INSTAGRAM_URL = "https://www.instagram.com/beardness.andy/";
   public static final String TELEGRAM_URL = "https://t.me/beardness_andy";
   
